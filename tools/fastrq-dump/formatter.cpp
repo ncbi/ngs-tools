b59ac1ece4306e73e04946a1644dd90985007ab8
ksrodarmer <rodarme1@ncbi.nlm.nih.gov>
2015-11-16 14:53:55 -0500

    first check in of the c++ fastrq-dump code

---
+++ tools/fastrq-dump/formatter.cpp
@@ -0,0 +1,139 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+
+#include "formatter.hpp"
+
+#include <ngs/ReadIterator.hpp>
+
+using namespace ngs;
+
+namespace fastrq
+{
+    /*======================================================================
+     * Fastq Formatter
+     */
+    struct FastQFormatter : FastRQFormatter
+    {
+        virtual void dump ( String run_name, uint64_t spotID, const ReadIterator &reads, const StringRef &bases ) const;
+
+        FastQFormatter ()
+        {
+        }
+    };
+
+    FastRQFormatter * makeFastQFormatter ()
+    {
+        return new FastQFormatter ();
+    }
+
+    void FastQFormatter :: dump ( String run_name, uint64_t spotID, const ReadIterator &reads, const StringRef &bases ) const
+    {
+        StringRef read_name = reads . getReadName ();
+        StringRef read_id = reads . getReadId ();
+        StringRef qualities = reads . getReadQualities ();
+        
+        std :: cout 
+            << '@'
+            << run_name
+            << '.'
+            //<< i
+            << read_id
+            << ' '
+            << read_name
+            << " length="
+            << bases . size ()
+            << '\n'
+            << bases
+            << '\n'
+            << '+'
+            << run_name
+            << '.'
+            //<< i
+            << read_id
+            << ' '
+            << read_name
+            << " length="
+            << qualities . size ()
+            << '\n'
+            << qualities
+            << '\n'
+            ;
+    }
+
+    /*======================================================================
+     * Fasta Formatter
+     */
+    struct FastAFormatter : FastRQFormatter 
+    {
+        virtual void dump ( String run_name, uint64_t spotID, const ReadIterator &reads, const StringRef &bases ) const;
+
+        FastAFormatter ()
+        {
+        }
+           
+    };
+
+    FastRQFormatter * makeFastAFormatter ()
+    {
+        return new FastAFormatter ();
+    }
+
+    void FastAFormatter :: dump ( String run_name, uint64_t spotID, const ReadIterator &reads, const StringRef &bases ) const
+    {
+        const size_t limit = 70;
+
+        StringRef read_name = reads . getReadName ();
+
+        if ( bases . size () > 0 )
+        {
+            StringRef base_str = bases;
+
+            std :: cout 
+                << '>'
+                << run_name
+                << '.'
+                << spotID
+                << ' '
+                << read_name
+                << " length="
+                << base_str . size ()
+                << '\n'
+                ;
+            
+            while ( base_str.size () > limit )
+            {
+                StringRef sub = base_str.substr(0,limit);
+
+                base_str = base_str.substr ( limit );
+
+
+                std :: cout << sub << '\n';
+            }
+            std :: cout << base_str << '\n';
+        }        
+    }
+
+}
---
+++ tools/fastrq-dump/operation.cpp
@@ -0,0 +1,158 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+
+#include "operation.hpp"
+
+#include <ngs/StringRef.hpp>
+#include <ngs/ReadCollection.hpp>
+#include <ngs/ReadIterator.hpp>
+#include <ngs/Read.hpp>
+
+#include "formatter.hpp"
+#include "filter.hpp"
+
+using namespace ngs;
+
+namespace fastrq
+{
+     /*======================================================================
+     * FastRQOperation
+     */
+    FastRQOperation :: FastRQOperation ()
+    {}
+    FastRQOperation :: ~FastRQOperation ()
+    {}
+
+    void FastRQOperation :: run ( ngs :: ReadCollection & run, const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+        throw __PRETTY_FUNCTION__;
+    }
+
+    void FastRQOperation :: run ( ngs :: ReadCollection & run, uint64_t start_row, uint64_t num_rows,
+                                  const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+        throw  __PRETTY_FUNCTION__;
+    }
+
+    void FastRQOperation :: run ( ngs :: ReferenceSequence & ref, const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+        throw  __PRETTY_FUNCTION__;
+    }
+    void FastRQOperation :: run ( ngs :: ReferenceSequence & ref, uint64_t start_row, uint64_t num_rows,
+                                  const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+        throw  __PRETTY_FUNCTION__;
+    }
+
+     /*======================================================================
+     * ReadOperation
+     */
+
+    struct ReadOperation : FastRQOperation
+    {
+        // filter ok NULL
+        virtual void run ( ReadCollection & run, const FastRQFilter * filter, const FastRQFormatter * fmt ) const;
+        virtual void run ( ReadCollection & run, uint64_t start_row, uint64_t num_rows,
+                           const FastRQFilter * filter, const FastRQFormatter * fmt ) const {}
+
+        ReadOperation ()
+        : start_row ( 0 ), num_rows ( 0 )
+        {
+        }
+        
+        ReadOperation ( uint64_t _start_row, uint64_t _num_rows )
+        : start_row ( _start_row ), num_rows ( _num_rows )
+        {
+        }
+
+        uint64_t start_row, num_rows;
+    };
+
+    FastRQOperation * makeReadOperation ()
+    {
+        return new ReadOperation ();
+    }
+
+    FastRQOperation * makeReadOperation ( uint64_t start_row, uint64_t num_rows )
+    {
+        return new ReadOperation ( start_row, num_rows );
+    }
+
+    void ReadOperation :: run ( ReadCollection & run, const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+        uint64_t spotID = 1;
+        String run_name = run . getName ();
+        
+        ReadIterator reads = run . getReads ( Read :: all );
+        while ( reads . nextRead () )
+        {
+            StringRef bases = reads . getReadBases ();
+            if ( filter == 0 )
+                fmt -> dump ( run_name, spotID, reads, bases );
+            else
+            {
+                if ( filter -> pass ( reads, bases ) )
+                    fmt -> dump ( run_name, spotID, reads, bases );
+            }
+            
+            ++ spotID;
+        }
+    }
+
+    /*======================================================================
+     * FragmentOperation
+     */
+    struct RefSeqOperation : FastRQOperation
+    {
+        // filter ok NULL
+        virtual void run ( ReferenceSequence & ref, const FastRQFilter * filter, const FastRQFormatter * fmt ) const;
+        virtual void run ( ReferenceSequence & ref, uint64_t start_row, uint64_t num_rows,
+                           const FastRQFilter * filter, const FastRQFormatter * fmt ) const {}
+
+        
+        RefSeqOperation ()
+        : start_row ( 0 ), num_rows ( 0 )
+        {
+        }
+        
+        RefSeqOperation ( uint64_t _start_row, uint64_t _num_rows )
+        : start_row ( _start_row ), num_rows ( _num_rows )
+        {
+        }
+
+        uint64_t start_row, num_rows;
+    };
+
+    FastRQOperation * makeRefSeqOperation ()
+    {
+        return new RefSeqOperation ();
+    }
+
+    void RefSeqOperation :: run ( ReferenceSequence & ref, const FastRQFilter * filter, const FastRQFormatter * fmt ) const
+    {
+    }
+
+}
