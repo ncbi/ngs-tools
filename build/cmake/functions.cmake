5289c8aed4a257cb715c569b2ea9ac02352d0601
Andrii Nikitiuk <andrii.nikitiuk@nih.gov>
2016-07-13 18:06:24 -0400

    VDB-3023 Added copying of pdb files for Debug builds

--- build/cmake/functions.cmake
+++ build/cmake/functions.cmake
@@ -4,11 +4,13 @@ function(ncbi_copy_exec_to_old_outdir target_name)
     if (NGS_TOOLS_OUTDIR_ENABLED)
         if (UNIX)
             add_custom_command(TARGET ${target_name} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${NGS_TOOLS_OUTDIR_PREFIX}/${OS}/${COMPILER}/${PLATFORM}/${BUILD}/bin/
                 COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target_name}> ${NGS_TOOLS_OUTDIR_PREFIX}/${OS}/${COMPILER}/${PLATFORM}/${BUILD}/bin/$<TARGET_FILE_NAME:${target_name}>
             )
         elseif (WIN32)
             add_custom_command(TARGET ${target_name} POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target_name}> ${NGS_TOOLS_OUTDIR_PREFIX}/${OS}/${PLATFORM_TOOLSET}/${WIN_PLATFORM}/$<CONFIGURATION>/bin/$<TARGET_FILE_NAME:${target_name}>
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${NGS_TOOLS_OUTDIR_PREFIX}/${OS}/${PLATFORM_TOOLSET}/${WIN_PLATFORM}/$<CONFIGURATION>/bin/
+                COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target_name}> $<$<CONFIG:Debug>:$<TARGET_PDB_FILE:${target_name}>> ${NGS_TOOLS_OUTDIR_PREFIX}/${OS}/${PLATFORM_TOOLSET}/${WIN_PLATFORM}/$<CONFIGURATION>/bin/
             )
         endif()
     endif()
