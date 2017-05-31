35ead2fbaddd8df10dddad95950e9438eb6312a6
Shutov, Oleg <oleg.shutov@nih.gov>
2017-05-31 14:36:25 -0400

    min window size parameter

--- tools/tax/examples/build_db_from_sequence_tree.sh
+++ tools/tax/examples/build_db_from_sequence_tree.sh
@@ -2,7 +2,7 @@ set -e
 bin_dir=../bin
 name=example
 du -h -a -b . | grep "sequence_tree.*\.fasta$" | sort -n -r > ./files.list.$name
-$bin_dir/build_index ./files.list.$name ./example_data/tax.parents 1000 32 > ./$name.kmers 2>./$name.kmers.log
+$bin_dir/build_index ./files.list.$name ./example_data/tax.parents 1000 32 64 > ./$name.kmers 2>./$name.kmers.log
 $bin_dir/check_index ./files.list.$name ./example_data/tax.parents ./$name.kmers > ./$name.kmers.checked 2> ./$name.kmers.checked.log
 $bin_dir/filter_db ./$name.kmers.checked > ./$name.kmers.checked.clean 2> ./$name.kmers.checked.clean.log
 $bin_dir/db_fasta_to_bin ./$name.kmers.checked.clean ./$name.dbs
