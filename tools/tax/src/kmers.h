c62d866d25a8013fe53e246724af137b6c3ace80
Shutov, Oleg <oleg.shutov@nih.gov>
2018-03-23 15:01:15 -0400

    removing ram reservation for very large files

--- tools/tax/src/kmers.h
+++ tools/tax/src/kmers.h
@@ -43,7 +43,7 @@ struct Kmers
 
 	Kmers(const TaxIdTree &tax_id_tree) : tax_id_tree(tax_id_tree)
 	{
-		storage.reserve(16L*1024*1024*1024); // todo: tune
+//		storage.reserve(4L*1024*1024*1024); // todo: tune
 	}
 
 	bool has_kmer(hash_t kmer) const
