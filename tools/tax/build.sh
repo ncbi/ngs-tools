27500de5acbf7efdaad0a307aaa560ddc6bc4f0a
Shutov, Oleg <oleg.shutov@nih.gov>
2021-02-02 13:04:18 -0500

    NGS build script and readme file

---
+++ tools/tax/build.sh
@@ -0,0 +1,19 @@
+sudo apt install git
+sudo apt install make 
+sudo apt install cmake
+sudo apt install default-jdk
+git clone https://github.com/ncbi/ngs-tools.git --branch tax
+git clone https://github.com/ncbi/ncbi-vdb.git
+git clone https://github.com/ncbi/ngs.git
+./ngs/configure --build-prefix=~/ncbi-outdir --without-debug
+cd ./ngs/
+make
+
+./ncbi-vdb/configure --build-prefix=~/ncbi-outdir --without-debug --with-ngs-sdk-prefix=~/ncbi-outdir
+cd ./ncbi-vdb/
+make
+./ngs-tools/configure '--without-debug' '--with-ngs-sdk-prefix=~/ncbi-outdir' '--with-ncbi-vdb-build=~/ncbi-outdir'
+cd ./ngs-tools/tools/tax
+make 
+
+cp ~/ncbi-outdir/ngs-tools/linux/gcc/x86_64/rel/bin/* ./ngs-tools/tools/tax/bin
---
+++ tools/tax/readme.txt
@@ -0,0 +1,5 @@
+Quick start guide:
+	* Run quickbuild.sh to build minimum files and functionality.
+	* Go to ./examples folder and run build_db_and_run.sh shell script. It has comments about each step performed.
+
+If you need NGS library support and all the tools you can run ./build.sh from any folder. 
