faaffc205cc2aba9adef9d32ab184dfa2dbd8ab1
Shutov, Oleg <oleg.shutov@nih.gov>
2021-02-02 12:24:01 -0500

    quickbuild.sh script

---
+++ tools/tax/quickbuild.sh
@@ -0,0 +1,8 @@
+set -e
+g++ -std=c++11 -O3 -fopenmp ./src/aligns_to.cpp ./src/reader.cpp -o ./bin/aligns_to -Wreturn-type -DNO_NGS_SUPPORT
+g++ -std=c++11 -O3 -fopenmp ./src/build_index_of_each_file.cpp -o ./bin/build_index_of_each_file -Wreturn-type
+g++ -std=c++11 -O3 -fopenmp ./src/merge_db.cpp -o ./bin/merge_db -Wreturn-type
+g++ -std=c++11 -O3 -fopenmp ./src/identify_tax_ids.cpp -o ./bin/identify_tax_ids -Wreturn-type
+g++ -std=c++11 -O3 -fopenmp ./src/db_tax_id_to_dbs.cpp -o ./bin/db_tax_id_to_dbs -Wreturn-type
+g++ -std=c++11 -O3 -fopenmp ./src/sort_dbs.cpp -o ./bin/sort_dbs -Wreturn-type
+echo "SUCCESS"
