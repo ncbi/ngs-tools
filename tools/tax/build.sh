d73e75d6ff51ac589d64e2113bba60deb496cb1a
Oleg Shutov <oleg.shutov@nih.gov>
2021-04-23 14:29:39 +0000

    SRA-9869: fix tax tools build script

--- tools/tax/build.sh
+++ tools/tax/build.sh
@@ -1,23 +1,31 @@
+sudo apt update
+sudo apt install build-essentials
+sudo apt install g++
 sudo apt install git
-sudo apt install make 
+sudo apt install make
 sudo apt install cmake
-#sudo apt install default-jdk
+sudo apt install default-jdk
+sudo apt install ant
+
 git clone https://github.com/ncbi/ngs-tools.git --branch tax
 git clone https://github.com/ncbi/ncbi-vdb.git
 git clone https://github.com/ncbi/ngs.git
-./ngs/configure --build-prefix=~/ncbi-outdir --without-debug
 cd ./ngs/
+./configure --without-debug
 make
 cd ..
 
-./ncbi-vdb/configure --build-prefix=~/ncbi-outdir --without-debug --with-ngs-sdk-prefix=~/ncbi-outdir
 cd ./ncbi-vdb/
+./configure --without-debug
 make
 cd ..
 
-./ngs-tools/configure '--without-debug' '--with-ngs-sdk-prefix=~/ncbi-outdir' '--with-ncbi-vdb-build=~/ncbi-outdir'
-cd ./ngs-tools/tools/tax
-make 
+cd ./ngs-tools/
+./configure --without-debug
+cd ./tools/tax
+./configure --without-debug
+make
+make
 cd ../../../
 
 cp ~/ncbi-outdir/ngs-tools/linux/gcc/x86_64/rel/bin/* ./ngs-tools/tools/tax/bin
