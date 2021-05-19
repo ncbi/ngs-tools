f6177e70cc4764ba15a45f7529947b91ea6bd3ed
Kenneth Durbrow <kmdurbrow@me.com>
2021-05-19 13:04:13 -0400

    Bump version

--- tools/tax/src/omp_adapter.h
+++ tools/tax/src/omp_adapter.h
@@ -32,4 +32,5 @@
    inline int omp_get_max_threads() { return 0; }
    inline int omp_get_thread_num() { return 0; }
    inline int omp_get_num_threads() { return 1; }
+   inline void omp_set_num_threads(int) {}
 #endif
