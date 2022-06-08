fff8b1c7879e74f6aec4923d3f43c41dfbe713c5
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2022-06-08 12:13:43 -0400

    SRAB-235, add option for unique

--- tools/tax/src/config_align_to.h
+++ tools/tax/src/config_align_to.h
@@ -40,7 +40,7 @@ struct Config
     std::string reference, db, dbs, dbsm, dbss, many, dbss_tax_list, spot_filter_file, out;
     std::list <std::string> contig_files;
 
-    bool unaligned_only = false;
+    bool unaligned_only = false, unique = false;
     bool hide_counts = false, compact = false;
 
     int optimization_ultrafast_skip_reader = 0;
@@ -91,6 +91,8 @@ struct Config
                 optimization_dbs_max_lookups_per_seq_fragment = std::stoi(pop_arg(args));
             else if (arg == "-num_threads")
                 num_threads = std::stoi(pop_arg(args));
+            else if (arg == "-unique")
+                unique = true;
             else if (arg.empty() || arg[0] == '-' || !contig_file.empty()) 
             {
                 std::string reason = "unexpected argument: " + arg;
@@ -154,7 +156,7 @@ struct Config
 
     static void print_usage()
     {
-        std::cerr << "need <database> [-spot_filter <spot or read file>] [-out <filename>] [-hide_counts] [-compact] [-unaligned_only] [-num_threads <number>] <contig fasta, accession or .list file of fasta/accessions>" << std::endl 
+        std::cerr << "need <database> [-spot_filter <spot or read file>] [-out <filename>] [-hide_counts] [-compact] [-unaligned_only] [-num_threads <number>] [-unique] <contig fasta, accession or .list file of fasta/accessions>" << std::endl
             << "where <database> is one of:" << std::endl
             << "-db <database>" << std::endl
             << "-dbs <database +tax>" << std::endl
