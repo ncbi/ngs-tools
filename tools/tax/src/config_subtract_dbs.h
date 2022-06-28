495c957457974940738a7348d00b960b3c86e50c
Shutov, Oleg <oleg.shutov@nih.gov>
2022-06-28 14:35:25 -0400

    SRAB-434: need a tool to verify that all kmers from one db (like human scrubber) are included into another db (like big db)

---
+++ tools/tax/src/config_subtract_dbs.h
@@ -0,0 +1,62 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+#pragma once
+
+#include <string>
+#include <iostream>
+
+struct Config
+{
+	std::string db_a, db_b;
+	int argc;
+	char const **argv;
+
+	std::string arg(int index) const
+	{
+		if (index >= argc)
+			fail();
+
+		return std::string(argv[index]);
+	}
+
+	Config(int argc, char const *argv[]) : argc(argc), argv(argv)
+	{
+		db_a = arg(1);
+		db_b = arg(2);
+	}
+
+	void fail() const
+	{
+		print_usage();
+        exit(1);
+	}
+
+	static void print_usage()
+	{
+		std::cerr << "need <a.dbs> <b.db>" << std::endl;
+	}
+};
+
