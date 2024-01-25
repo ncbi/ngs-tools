84dbcb31a92d88013e2a233c3e295cf7dac62bde
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2024-01-25 14:50:23 -0500

    SRAB-1124, updated to python 3

--- tools/tax/bin/tax_analysis_parser.py
+++ tools/tax/bin/tax_analysis_parser.py
@@ -243,7 +243,7 @@ With this flag parser builds hierarchy based on count of kmer hits, not the coun
     with gettax.connect(args.tax_dump, args.sqlite_cache, args.rebuild_timeout, args.connection_timeout) as conn:
         xml = parse(f, conn, args.wgs_mode, args.compact, args.collated, args.include_tax_id or [])
     xml = E.taxon_tree(xml, parser_version=__version__)
-    print etree.tostring(xml, pretty_print=True)
+    print (etree.tostring(xml, pretty_print=True))
 
 if __name__ == '__main__':
     main()
