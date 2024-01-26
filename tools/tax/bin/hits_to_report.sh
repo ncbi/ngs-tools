14aa7742962501905329c3dd3219da0dc29c7ff9
Katz, Kenneth <kskatz@ncbi.nlm.nih.gov>
2024-01-26 15:10:16 -0500

    SRAB-1124, remove summary report

--- tools/tax/bin/hits_to_report.sh
+++ tools/tax/bin/hits_to_report.sh
@@ -13,5 +13,4 @@ python3 -m venv $_tmpdir/venv
 source $_tmpdir/venv/bin/activate
 pip install -r $script_dir/requirements.txt
 python3 $script_dir/tax_analysis_parser.py  -c ./gettax.sqlite $hits.sorted > $hits.sorted.report.xml
-python3 $script_dir/tax_analysis_report.py $hits.sorted.report.xml
 deactivate
